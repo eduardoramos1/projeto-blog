90e285a8bc1a0be13053c9117b34ada6d5275610
Eduardo Ramos <eduardorxs@gmail.com>
2019-08-06 01:51:46 -0300

    bla

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 //Verifica se a aplicação está em ambiente de produção
 if(process.env.NODE_ENV == 'production'){
     module.exports = {
-        mongoURI: 'mongodb+srv://eduardoramos1:eduardo1995@cluster0-ojls3.mongodb.net/test'
+        mongoURI: 'mongodb+srv://eduardoramos1:<password>@cluster0-ojls3.mongodb.net/test?retryWrites=true&w=majority'
     }
 } else {
     module.exports = {
